ea42b25debe0bffa2e2eeab32b0d86a52439f695
stevapple <stevapple@icloud.com>
2023-02-02 00:15:52 +0800

    Moves `TecoRegionGenerator` to use code generation

--- Sources/TecoRegionGenerator/helpers.swift
+++ Sources/TecoRegionGenerator/helpers.swift
@@ -1,24 +1,3 @@
-@_implementationOnly import OrderedCollections
-
-typealias RegionId = String
-
-func getRegionMap(from regionInfo: [RegionInfo]) -> OrderedDictionary<RegionId, String> {
-    let regions = regionInfo.map { ($0.region, $0.regionName) }
-    return .init(uncheckedUniqueKeysWithValues: regions)
-}
-
-func getRegions(from maps: OrderedDictionary<RegionId, String>...) -> OrderedSet<Region> {
-    let baseKeys = maps[0].keys
-    if maps.count > 1 {
-        for map in maps[1...] {
-            precondition(map.keys == baseKeys)
-        }
-    }
-    return OrderedSet(baseKeys.map { id in
-        Region(id: id, localizedNames: OrderedSet(maps.map { $0[id]! }))
-    })
-}
-
 extension Region: Equatable, Comparable {
     var description: String { self.localizedNames.joined(separator: " / ") }
     var identifier: String { self.id.replacingOccurrences(of: "-", with: "_") }
