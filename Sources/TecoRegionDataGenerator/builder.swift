1ff79fb50b9aad24a295363e22eaaa4726ff3096
YR Chen <stevapple@icloud.com>
2023-04-06 21:30:44 +0800

    Update `TecoRegionDataGenerator`

--- Sources/TecoRegionDataGenerator/builder.swift
+++ Sources/TecoRegionDataGenerator/builder.swift
@@ -4,22 +4,17 @@ import SwiftSyntaxBuilder
 
 func buildRegionExpr(id: String, names: OrderedSet<String>, trailingComma: Bool = false) -> ArrayElementSyntax {
     precondition(names.isEmpty == false)
-    let valueExpr = FunctionCallExprSyntax(callee: ExprSyntax("Region")) {
-        TupleExprElementSyntax(label: "id", expression: ExprSyntax("\(literal: id)"))
-        TupleExprElementSyntax(label: "localized", expression: ExprSyntax("\(literal: Array(names))"))
-    }
+    let valueExpr = ExprSyntax("Region(id: \(literal: id), localized: \(literal: Array(names)))")
     return ArrayElementSyntax(expression: valueExpr, trailingComma: trailingComma ? .commaToken() : nil)
 }
 
 func buildRegionListExpr(from maps: OrderedDictionary<String, String>...) -> ArrayExprSyntax {
-    let baseKeys = maps[0].keys
-    for map in maps.dropFirst() {
-        precondition(map.keys == baseKeys)
-    }
-    return ArrayExprSyntax(elements: ArrayElementListSyntax {
-        for id in baseKeys {
-            buildRegionExpr(id: id, names: .init(maps.map { $0[id]! }), trailingComma: true)
-                .withTrailingTrivia(.newline)
+    precondition(Set(maps.map(\.keys)).count == 1)
+    return ArrayExprSyntax {
+        for (index, region) in maps[0].keys.enumerated() {
+            buildRegionExpr(id: region, names: .init(maps.map { $0[region]! }), trailingComma: true)
+                .with(\.trailingTrivia, .newline)
+                .with(\.leadingTrivia, index == 0 ? .newline : nil)
         }
-    }.withLeadingTrivia(.newline))
+    }
 }
