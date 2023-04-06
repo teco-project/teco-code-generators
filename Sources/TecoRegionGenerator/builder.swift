1222fb9c93cc303dca83da2545294257dd5f4124
YR Chen <stevapple@icloud.com>
2023-04-06 21:36:36 +0800

    Update `TecoRegionGenerator`

--- Sources/TecoRegionGenerator/builder.swift
+++ Sources/TecoRegionGenerator/builder.swift
@@ -3,9 +3,9 @@ import SwiftSyntaxBuilder
 
 func buildRegionExpr(for region: Region) -> FunctionCallExprSyntax {
     FunctionCallExprSyntax(callee: ExprSyntax("TCRegion")) {
-        TupleExprElementSyntax(label: "id", expression: StringLiteralExprSyntax(content: region.id))
+        TupleExprElementSyntax(label: "id", expression: ExprSyntax(literal: region.id))
         if let kind = region.kind {
-            TupleExprElementSyntax(label: "kind", expression: MemberAccessExprSyntax(name: kind))
+            TupleExprElementSyntax(label: "kind", expression: ExprSyntax(".\(raw: kind)"))
         }
     }
 }
