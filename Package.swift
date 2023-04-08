99c036e4b58deb415b81fd960e6c385faf002609
YR Chen <stevapple@icloud.com>
2023-04-08 20:22:18 +0800

    Unblocks Swift 5.8 compatibility

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-syntax.git", exact: "509.0.0-swift-5.9-DEVELOPMENT-SNAPSHOT-2023-03-27-a"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0"),
-        .package(url: "https://github.com/teco-project/teco-core.git", from: "0.4.0"),
+        .package(url: "https://github.com/teco-project/teco-core.git", .upToNextMinor(from: "0.5.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
